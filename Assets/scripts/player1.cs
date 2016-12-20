38a4f3c9a03c5109794dbbd3731b7a08aebe32c2
zhangzhao <1578224594@qq.com>
2016-12-20 20:56:38 +0800

    chushiban

---
+++ Assets/scripts/player1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1 : MonoBehaviour {
+	public float playerspeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.W)) {  
+			print ("w");  
+			transform.Translate (Vector3.up * Time.deltaTime * playerspeed);  
+		}  
+
+		//向下运动——S  
+		if (Input.GetKey (KeyCode.S)) {  
+			print ("s");  
+			transform.Translate (Vector3.down * Time.deltaTime * playerspeed);  
+		}  
+
+		//向左运动——A  
+		if (Input.GetKey (KeyCode.A)) {  
+			print ("a");  
+			transform.Translate (Vector3.left * Time.deltaTime * playerspeed);  
+		}  
+
+		//向右运动——D  
+		if (Input.GetKey (KeyCode.D)) {  
+			print ("d");  
+			transform.Translate (Vector3.right * Time.deltaTime * playerspeed);  
+		}  
+		if(Input.GetMouseButton(0)){
+			transform.Rotate(Vector3.down* playerspeed,Space.World);
+		}
+		//鼠标判断
+		if(Input.GetMouseButton(1)){
+			transform.Rotate(Vector3.up* playerspeed,Space.World);
+		}
+
+	}
+}
---
+++ Assets/scripts/player2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2 : MonoBehaviour {
+	private bool isMouseDown = false;  
+	private Vector3 lastMousePosition = Vector3.zero;  
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))  
+		{  
+			isMouseDown = true;
+			Debug.Log("Pressed left click.");
+		}  
+		if (Input.GetMouseButtonUp(0))  
+		{  
+			isMouseDown = false; 
+			lastMousePosition = Vector3.zero;
+			Debug.Log("Pressed ");
+		}  
+		if (isMouseDown)  
+		{  
+			if (lastMousePosition != Vector3.zero)  
+			{  
+				Vector3 offset = Camera.main.ScreenToWorldPoint(Input.mousePosition) - lastMousePosition;  
+				this.transform.position += offset;  
+			}  
+			lastMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);  
+
+		}  
+
+	}
+
+}
